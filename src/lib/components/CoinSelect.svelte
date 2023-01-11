8f5e147b5b505c8f10679d5ded6f34b3da1c0f66
AlexeySadkovich <sadkovich01@mail.ru>
2023-01-11 20:12:11 +0300

    initial commit

---
+++ src/lib/components/CoinSelect.svelte
@@ -0,0 +1,65 @@
+<script>
+	import ETHLogo from '$lib/images/eth_logo.png';
+	import TZSLogo from '$lib/images/tzs_logo.svg';
+	import TONLogo from '$lib/images/ton_logo.svg';
+	import { writable } from 'svelte/store';
+
+	let coins = [
+		{
+			symbol: 'ETH',
+			logo: ETHLogo,
+			size: 20
+		},
+		{
+			symbol: 'TZS',
+			logo: TZSLogo,
+			size: 20
+		},
+		{
+			symbol: 'TON',
+			logo: TONLogo,
+			size: 25
+		}
+	];
+
+	export let selectedId = writable(0);
+
+	const selectCoin = (id) => {
+		selectedId.set(id);
+	};
+</script>
+
+<div class="dropdown w-full">
+	<label
+		tabindex="0"
+		class="flex-shrink-0 z-10 inline-flex items-center justify-between py-2.5 px-4 text-sm font-medium text-center text-gray-500 bg-base-100 w-full h-14"
+	>
+		<div class="flex flex-row gap-5 items-center">
+			<img src={coins[$selectedId].logo} alt="" width={coins[$selectedId].size} />
+			{coins[$selectedId].symbol}
+		</div>
+		<svg aria-hidden="true" class="w-4 h-4" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"
+			><path
+				fill-rule="evenodd"
+				d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+				clip-rule="evenodd"
+			/></svg
+		>
+	</label>
+	<ul tabindex="0" class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-full">
+		{#each coins as coin, id}
+			{#if id !== $selectedId}
+				<li>
+					<button
+						on:click={() => {
+							selectCoin(id);
+						}}
+					>
+						<img class="mr-2" src={coin.logo} alt="" width={coin.size} />
+						{coin.symbol}
+					</button>
+				</li>
+			{/if}
+		{/each}
+	</ul>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,44 @@
+<script>
+	import { fly } from 'svelte/transition';
+	import { onMount } from 'svelte';
+
+	import Arrows from '$lib/images/arrows.svg';
+	import CoinSelect from '$lib/components/CoinSelect.svelte';
+	import { writable } from 'svelte/store';
+
+	let ready = false;
+	onMount(() => (ready = true));
+
+	let fromCoin = writable(0);
+	let toCoin = writable(1);
+</script>
+
+<div class="flex flex-col md:flex-row h-screen justify-center items-center bg-base-200">
+	<div class="hero md:h-full">
+		<div class="hero-content text-left">
+			{#if ready}
+				<div class="max-w-xl" in:fly={{ x: -200, duration: 1500 }}>
+					<h1 class="text-5xl font-bold">👾 Bifrost</h1>
+					<p class="py-8">Swap tokens between Ethereum, TON and Tezos with ease.</p>
+				</div>
+			{/if}
+		</div>
+	</div>
+	<div class="container flex justify-center items-center">
+		<div
+			class="bg-base-300 shadow-xl flex flex-col items-center gap-5 py-8 mx-10 px-5 w-96 border-4 border-black"
+		>
+			<h4>Wrapped Swap</h4>
+			<div class="w-full">
+				<p>from</p>
+				<CoinSelect selectedId={fromCoin} />
+			</div>
+			<button><img src={Arrows} width={30} alt="arrows" /></button>
+			<div class="w-full">
+				<p>to</p>
+				<CoinSelect selectedId={toCoin} />
+			</div>
+			<a class="btn btn-primary btn-wide" href="#">go swap!</a>
+		</div>
+	</div>
+</div>
