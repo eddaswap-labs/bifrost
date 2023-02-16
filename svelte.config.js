dfd89797963c5104e25dcc7216789f83903d671d
AlexeySadkovich <sadkovich01@mail.ru>
2023-02-16 10:24:34 +0300

    build: remove paths base

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,12 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-const dev = process.argv.includes('dev');
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 	kit: {
-		paths: {
-			base: dev ? '' : '/bifrost'
-		},
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
